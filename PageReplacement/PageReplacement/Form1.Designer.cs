9770dafee124d81220335f9be142cebda64b85de
FTD <zxc2012@gmail.com>
2014-09-21 16:34:07 +0800

    OPT with bugs.

--- PageReplacement/PageReplacement/Form1.Designer.cs
+++ PageReplacement/PageReplacement/Form1.Designer.cs
@@ -28,12 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- PageReplacement/PageReplacement/Form1.cs
+++ PageReplacement/PageReplacement/Form1.cs
@@ -12,16 +12,20 @@ namespace PageReplacement
 {
     public partial class Form1 : Form
     {
-        public static delegate void ReplaceMethod();
+        private static int[] addressStream = new int[500];
+
+        public delegate int ReplaceMethod(int frameCount);
         public Form1()
         {
             InitializeComponent();
+            MainFunction();
         }
 
         private void MainFunction()
         {
+            Queue<int> optResult = new Queue<int>();
+            Queue<int> fifoResult = new Queue<int>();
             Random re = new Random();
-            int[] address = new int[500];
 
             ReplaceMethod opt = new ReplaceMethod(OPT);
             ReplaceMethod fifo = new ReplaceMethod(FIFO);
@@ -29,33 +33,125 @@ namespace PageReplacement
 
             for (int i = 0; i < 400; i++ )
             {
-                address[i] = re.Next(0, 400);            
+                //addressStream[i] = i % 40;
+                addressStream[i] = re.Next(0, 399) % 40;            
             }
             for (int i = 4; i <= 40; i++)
             {
-                FrameAndMethodSeletion(i, opt);
-                FrameAndMethodSeletion(i, fifo);
-                FrameAndMethodSeletion(i, lru);
+                optResult.Enqueue(FrameAndMethodSeletion(i, opt));
+                fifoResult.Enqueue(FrameAndMethodSeletion(i, fifo));
+                //FrameAndMethodSeletion(i, lru);
             }
-            
         }
 
-        private void FrameAndMethodSeletion(int i, ReplaceMethod rm)
+        private int FrameAndMethodSeletion(int i, ReplaceMethod rm)
         {
-            
+            return rm(i); 
         }
 
-        private static void OPT()
+        #region OPT置换算法
+        private static int OPT(int frameCount)
+        {
+            int frameLack = 0;
+            int[] frame = new int[frameCount];
+            for (int i = 0; i < frameCount; i++ )
+            {
+                frame[i] = -1;
+            }
+            for (int pos = 0; pos < 400; pos++)
+            {
+                bool isContain = false;
+                isContain = IsPageInFrame(ref frame, frameCount, pos); 
+                if (!isContain)
+                {
+                    frameLack++;
+                    int farOrNerver = -1;
+                    //淘汰永不使用或下次访问距当前时间最长的页面
+                    for (int i = 0; i < frameCount; i++ )
+                    {
+                        for (int j = pos; j < 400; j++)
+                        {
+                            if (frame[i] == addressStream[j] && farOrNerver < j)
+                            {
+                                farOrNerver = j;
+                                break;
+                            }
+                        }
+                        if (frame[i] == -1)
+                        {
+                            farOrNerver = -1;
+                            frame[i] = addressStream[pos];
+                            break;
+                        }
+                    }
+                    if (farOrNerver != -1)
+                    {
+                        for (int i = 0; i < frameCount; i++)
+                        {
+                            if (frame[i] == addressStream[farOrNerver])
+                            {
+                                frame[i] = addressStream[pos];
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            return frameLack;
+        }
+        #endregion
+
+        #region FIFO算法
+        private static int FIFO(int frameCount)
+        {
+            int frameLack = 0;
+            int currentFrame = 0;
+            int[] frame = new int[frameCount];
+            for (int i = 0; i < frameCount; i++ )
+            {
+                frame[i] = -1;
+            }
+            for (int pos = 0; pos < 400; pos++ )
+            {
+                currentFrame %= frameCount;
+                bool isContain = false;
+                isContain = IsPageInFrame(ref frame, frameCount, pos);
+                if (!isContain)
+                {
+                    frameLack++;
+                    //淘汰最先进的页
+                    frame[currentFrame++] = addressStream[pos]; 
+                }
+            }
+            return frameLack;
+        }
+        #endregion
+
+        private static int LRU(int frameCount)
+        {
+            return 0;
+        }
+
+        #region 帧内是否包含下一个页
+        private static bool IsPageInFrame(ref int[] frame, int frameCount, int pos)
+        {
+            for (int i = 0; i < frameCount; i++)
+            {
+                if (frame[i] == addressStream[pos])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        #endregion
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
 
-        private static void FIFO()
-        {
-
-        }
-
-        private static void LRU()
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }

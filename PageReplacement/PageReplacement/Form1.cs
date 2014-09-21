37821d9027f27b674f4e2eaf284a9c1bca463553
FTD <zxc2012@gmail.com>
2014-09-21 10:18:01 +0800

    Basic Frame.

--- PageReplacement/PageReplacement/Form1.cs
+++ PageReplacement/PageReplacement/Form1.cs
@@ -12,10 +12,52 @@ namespace PageReplacement
 {
     public partial class Form1 : Form
     {
+        public static delegate void ReplaceMethod();
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void MainFunction()
+        {
+            Random re = new Random();
+            int[] address = new int[500];
+
+            ReplaceMethod opt = new ReplaceMethod(OPT);
+            ReplaceMethod fifo = new ReplaceMethod(FIFO);
+            ReplaceMethod lru = new ReplaceMethod(LRU);
+
+            for (int i = 0; i < 400; i++ )
+            {
+                address[i] = re.Next(0, 400);            
+            }
+            for (int i = 4; i <= 40; i++)
+            {
+                FrameAndMethodSeletion(i, opt);
+                FrameAndMethodSeletion(i, fifo);
+                FrameAndMethodSeletion(i, lru);
+            }
+            
+        }
+
+        private void FrameAndMethodSeletion(int i, ReplaceMethod rm)
+        {
+            
+        }
+
+        private static void OPT()
+        {
+
+        }
+
+        private static void FIFO()
+        {
+
+        }
+
+        private static void LRU()
+        {
+
+        }
     }
 }
